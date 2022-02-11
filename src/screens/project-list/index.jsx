9fca6ed6dbfe2404fb64c5b8344f041294a895d5
yifan <372104922@qq.com>
2022-02-11 21:47:36 +0800

    feat: 添加useMount 和 useDebounce 两个hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { List } from "./list";
 import { SearchPanel } from "./search-panel";
 import * as qs from "qs";
-import { cleanObject } from "utils";
+import { cleanObject, useDebounce, useMount } from "utils";
 
 const apiUrl = process.env.REACT_APP_API_URL
 
@@ -15,23 +15,24 @@ export const ProjectListScreen = ()=>{
         name:'',
         personId:''
     })
+    const debounceParam = useDebounce(param, 2000)
     const [list, setList] = useState([])
 
     useEffect(()=>{
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response =>{
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async response =>{
             if (response.ok){
                 setList(await response.json());
             }
         })
-    }, [param])
+    }, [debounceParam])
 
-    useEffect(()=>{
+    useMount(()=>{
         fetch(`${apiUrl}/users`).then(async response =>{
             if (response.ok){
                 setUsers(await response.json());
             }
         })
-    }, [])
+    })
 
 
     return <div>
