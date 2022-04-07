5d6c47c7099d7484260b3033fe322a2cc4df5254
Yifan <372104922@qq.com>
2022-04-07 19:50:53 +0800

    feat: 添加登录维持

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,42 +1,38 @@
-import React from "react"
+import React from "react";
+import { SearchPanel } from "screens/project-list/search-panel";
+import { List } from "screens/project-list/list";
 import { useEffect, useState } from "react";
-import { List } from "./list";
-import { SearchPanel } from "./search-panel";
+import { cleanObject, useDebounce, useMount } from "../../utils";
 import * as qs from "qs";
-import { cleanObject, useDebounce, useMount } from "utils";
+import { useHttp } from "utils/http";
 
-const apiUrl = process.env.REACT_APP_API_URL
+// 使用 JS 的同学，大部分的错误都是在 runtime(运行时) 的时候发现的
+// 我们希望，在静态代码中，就能找到其中的一些错误 -> 强类型
+const apiUrl = process.env.REACT_APP_API_URL;
 
-export const ProjectListScreen = ()=>{
+export const ProjectListScreen = () => {
+  const [users, setUsers] = useState([]);
 
-    const[users, setUsers] = useState([])
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const debouncedParam = useDebounce(param, 200);
+  const [list, setList] = useState([]);
+  const client = useHttp();
 
-    const [param, setParam] = useState({
-        name:'',
-        personId:''
-    })
-    const debounceParam = useDebounce(param, 200)
-    const [list, setList] = useState([])
+  useEffect(() => {
+    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
+  }, [debouncedParam]);
 
-    useEffect(()=>{
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async response =>{
-            if (response.ok){
-                setList(await response.json());
-            }
-        });
-    }, [debounceParam])
+  useMount(() => {
+    client("users").then(setUsers);
+  });
 
-    useMount(()=>{
-        fetch(`${apiUrl}/users`).then(async response =>{
-            if (response.ok){
-                setUsers(await response.json());
-            }
-        });
-    })
-
-
-    return <div>
-        <SearchPanel users={users} param={param} setParam={setParam}/>
-        <List users={users} list={list}/>
+  return (
+    <div>
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List users={users} list={list} />
     </div>
-}
+  );
+};
