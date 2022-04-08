e10d42278a1c267c5c7c25bc50abcecc5b448f4f
Yifan <372104922@qq.com>
2022-04-08 12:56:03 +0800

    feat: use antd

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {loadDevTools} from 'jira-dev-tool'
+import { loadDevTools } from 'jira-dev-tool'
+//务必在jira-dev-tool后
+import 'antd/dist/antd.less'
 import { AppProviders } from 'context';
 
 loadDevTools(() => ReactDOM.render(
--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,35 +1,43 @@
-import React from "react"
-import {User} from "screens/project-list/search-panel";
+import React from "react";
+import { User } from "screens/project-list/search-panel";
+import { Table } from "antd";
 
 interface Project {
   id: string;
   name: string;
   personId: string;
-  title: string;
+  pin: boolean;
   organization: string;
 }
 
 interface ListProps {
-  list: Project[],
-  users: User[]
+  list: Project[];
+  users: User[];
 }
 
-export const List = ({list, users}: ListProps)=>{
-  //函数体
-    return <table>
-      <thead>
-        <tr>
-          <th>名称</th>
-          <th>负责人</th>
-        </tr>
-      </thead>
-      <tbody>
+export const List = ({ list, users }: ListProps) => {
+  return (
+    <Table
+      pagination={false}
+      columns={[
         {
-          list.map(project => <tr key={project.id}>
-            <td>{project.name}</td>
-            <td>{users.find(user => user.id === project.personId)?.name || '未知'}</td>
-          </tr> )
-        }
-      </tbody>
-    </table>
-}
+          title: "名称",
+          dataIndex: "name",
+          sorter: (a, b) => a.name.localeCompare(b.name),
+        },
+        {
+          title: "负责人",
+          render(value, project) {
+            return (
+              <span>
+                {users.find((user) => user.id === project.personId)?.name ||
+                  "未知"}
+              </span>
+            );
+          },
+        },
+      ]}
+      dataSource={list}
+    />
+  );
+};
